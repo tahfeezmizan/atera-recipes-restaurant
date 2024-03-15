bd179755817bf97f437e61c52a7f375c9b9efe6b
tahfeezmizan <mizanahmed5316@gmial.com>
2024-03-16 00:18:41 +0600

    create banner component, and complete design

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,21 @@
+
+const Banner = () => {
+    return (
+        <div className="w-full md:w-3/4 lg:w-8/12 mx-auto pb-12 px-4 lg:px-0">
+            <div className="slider bg-img rounded-3xl h-[400px] lg:h-[600px]  flex flex-col items-center justify-center">
+                {/* banner text container  */}
+                <div className="text-center text-white ">
+                    <h1 className="text-4xl lg:text-5xl font-bold mb-7 px-1 lg:px-60">Discover an exceptional cooking class tailored for you!</h1>
+                    <p className="text-lg mb-10 px-1 lg:px-80">Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding problems to become an exceptionally well world-class Programmer.</p>
+                    <div className="flex gap-5 items-center justify-center">
+                        <button className="btn rounded-3xl px-8 border-none bg-green-400 text-xl font-semibold"><a
+                            href="#ticket-interface">Explore Now</a></button>
+                        <button className="btn btn-outline rounded-3xl px-8 border text-white border-white text-xl font-semibold">Our Feedback</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
-                        <ul tabindex="0"
+                        <ul
                             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                             <li className="text-lg font-medium text-secondaryColor opacity-70">Home</li>
                             <li className="text-lg font-medium text-secondaryColor opacity-70">Recipes</li>
@@ -34,10 +34,8 @@ const Header = () => {
                     {/* <div className="form-control">
                         <input type="text" placeholder="Search" className="input rounded-3xl bg-gray-100 border-none w-24 md:w-auto" />
                     </div> */}
-
-
                     <div className="">
-                        <input type="text" placeholder="Search" className=" input rounded-3xl bg-gray-100 w-24 md:w-auto"/>
+                        <input type="text" placeholder="Search" className=" input rounded-3xl bg-gray-100 w-24 md:w-auto" />
                         <img src="../../assets/Frame 5.png" alt="" />
                     </div>
                 </div>
