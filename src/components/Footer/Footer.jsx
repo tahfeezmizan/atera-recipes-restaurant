a021b8f477259b7df31166fbc0edc52d461cf2ce
Tahfeez Mizan <mizanahmed5316@gmail.com>
2024-07-08 10:03:32 -0700

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,13 +9,11 @@ const Footer = () => {
                 <h2 class="mulish text-4xl font-black mb-6">Atera Recipe</h2>
                 <p class="inter text-lg px-6 lg:px-40 xl:px-72 text-black text-opacity-60 ">Enjoy an exceptional journey of taste. It's our job every day to make every important aspect of the customer experience a little better.</p>
             </div>
-
             <div class="flex justify-center items-center gap-4 py-5">
                 <img src={facebookSvg} alt="SVG Image" />
                 <img src={twitterSvg} alt="SVG Image" />
                 <img src={instaSvg} alt="SVG Image" />
             </div>
-
             <div class="text-center pt-12 border-t-2">
                 <p class="mulish text-base font-light text-black text-opacity-60">2024, All Rights Reserved.</p>
             </div>
